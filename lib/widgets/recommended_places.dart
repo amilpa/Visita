57ab2e394151a81a2254c8ca75c662069ba4a636
Anish Pillai <anishp8914@gmail.com>
2023-04-28 09:23:53 +0530

    Update 1.0

--- lib/widgets/recommended_places.dart
+++ lib/widgets/recommended_places.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:visita/model/recommended_places_model.dart';
+import 'package:http/http.dart' as http;
 import 'package:visita/pages/tourist_details.dart';
 
 class RecommendedPlaces extends StatefulWidget {
